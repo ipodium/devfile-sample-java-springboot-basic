fb10d574f7b778e33f9268243a986f0b56cf0f63
Student User <student@workstation.lab.example.com>
2022-10-19 15:21:00 -0400

    hell world again and greetings

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World again! greetings to you all :-)";
     }
 
     public static void main(String[] args) {
